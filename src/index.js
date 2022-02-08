cc887131c5b8e977c0a7c836893c270496171d1f
Mikhail Druzhinin <pini86@mail.ru>
2022-02-08 18:09:34 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+     const open = new Set(bracketsConfig.map(pair => pair[0]));//открывающие скобки
+     const close  = new Set(bracketsConfig.map(pair => pair[1]));//закрывающие скобки
+     const relevant = bracketsConfig.reduce((acc, [open, close]) => ({...acc, [close]: open}), {});//массив соответствия закрывающей и открывающей
+     let stack = [];
+     for (let char of str) {
+       let topElem = stack[stack.length-1];//смотрим что на вершине стека, не снимая её
+       if (open.has(char) && close.has(char)) {//проверка : если открывающая = закрывающей
+         if (topElem === char){                 //если на вершине стека такая-же скобка
+          if (relevant[char] !== stack.pop()) return false;// то пробуем снимать и проверяем соответствие скобок
+         } else {
+          stack.push(char);  //иначе , кладем в стек
+         }
+       }
+       if (open.has(char) && !close.has(char)) {//проверка : только открывающая скобка
+         stack.push(char);                    //кладем в стек
+       }
+       if (close.has(char) && !open.has(char)) {  //проверка : только закрывающая скобка
+         if (relevant[char] !== stack.pop()) return false; // снимаем верхушку стека и проверяем соответствие скобок
+       }
+     }
+     return stack.length === 0; //осталось чего-нибудь в стеке
 }
